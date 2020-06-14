1cf4cc02019adacbcab58b00aeacdcab865a2c6a
Jamz <2452534+lamerock@users.noreply.github.com>
2020-06-15 00:03:56 +0800

    Update SCSS functions

--- lib.php
+++ lib.php
@@ -49,5 +49,12 @@ function theme_photo_get_main_scss_content($theme) {
         $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
     }
 
-    return $scss;
+	// Pre CSS - this is loaded AFTER any prescss from the setting but before the main scss.
+    $pre = file_get_contents($CFG->dirroot . '/theme/eskuela/scss/pre.scss');
+	
+    // Post CSS - this is loaded AFTER the main scss but before the extra scss from the setting.
+    $post = file_get_contents($CFG->dirroot . '/theme/eskuela/scss/post.scss');
+	
+     // Combine them together.
+    return $pre . "\n" . $scss . "\n" . $post;
 }
