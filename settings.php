1ab6e8e0af7cc55b25ef879ca23671c9cc3bf305
Jamz <2452534+lamerock@users.noreply.github.com>
2020-06-14 23:49:15 +0800

    Update settings.php

--- settings.php
+++ settings.php
@@ -94,4 +94,19 @@ if ($ADMIN->fulltree) {
     $page->add($setting);
 
     $settings->add($page);
+
+	// Login page background setting.
+    // We use variables for readability.
+    $name = 'theme_eskuela/loginbackgroundimage';
+    $title = get_string('loginbackgroundimage', 'theme_eskuela');
+    $description = get_string('loginbackgroundimage_desc', 'theme_eskuela');
+	
+    // This creates the new setting.
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'loginbackgroundimage');
+	
+    // This means that theme caches will automatically be cleared when this setting is changed.
+    $setting->set_updatedcallback('theme_reset_all_caches');
+	
+    // We always have to add the setting to a page for it to have any effect.
+    $page->add($setting);
 }
